7c69d1ca85c877499b1bcbd28882b2d42742d047
fdonkar <fdonkar@gmail.com>
2022-06-09 11:55:37 +0300

    fix compareVerification

--- cypress/support/models.js
+++ cypress/support/models.js
@@ -1,4 +1,4 @@
-import {modelSURL} from '../support/authentication.constant'
+import {modelSURL,comparePageUrl} from '../support/authentication.constant'
 var data = require('../fixtures/modelSPage.json')
 var compareData = require('../fixtures/compareModels.json')
 
@@ -18,9 +18,12 @@ export class ModelS{
         })
     }
 
+    // navigate to page https://www.tesla.com/models
+    // click compare 
+    // check every single text on the page 
     compareVerification(){
-        cy.scrollTo('bottomLeft')
         cy.get('[title="Compare"]').click()
+        cy.url().should('eq', comparePageUrl)
         cy.get('[class="tds-text--caption tcl-compare-models__item-title"]').each((el,index)=>{
             const text = el.text()
             expect(text).to.equal(compareData.CompareModels[index])
@@ -31,9 +34,7 @@ export class ModelS{
 
 }
 
-// navigate to page https://www.tesla.com/models
-//click compare 
-//check every single text on the page 
+
 
 
 export const onModelSPage = new ModelS;
